f998c1ec122fd84de3217ef3c3687047e340a86e
Daniel Justo <justo8162@gmail.com>
2026-05-12 11:31:55 -0300

    first commit

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = 'firebird:dbname=db:/firebird/data/agenda.fdb;charset=UTF8';
+$user = 'SYSDBA';
+$password = 'masterkey';
+
+echo "<h1>Teste de Conexão - Agenda Telefônica</h1>";
+
+try {
+    $pdo = new PDO($host, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "<p style='color: green;'>Sucesso! O PHP conseguiu se conectar ao Firebird.</p>";
+
+    $info = $pdo->getAttribute(PDO::ATTR_SERVER_VERSION);
+    echo "<p>Versão do Banco: $info</p>";
+
+} catch (PDOException $e) {
+    echo "<p style='color: red;'>Erro ao conectar: " . $e->getMessage() . "</p>";
+}
